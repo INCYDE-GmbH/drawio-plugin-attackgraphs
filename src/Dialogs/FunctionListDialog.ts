b45f9d589c4bd20917c50b78a37b85d2aab5398c
Tobias Wothge <tobi18991@users.noreply.github.com>
2022-05-23 12:03:37 +0200

    Improve design of FunctionListDialog
    * Enhancements in the code.
    * Adapt dialog size
    * Fix retrieval of mxResources for VERTEX_TYPE

--- src/Dialogs/FunctionListDialog.ts
+++ src/Dialogs/FunctionListDialog.ts
@@ -6,27 +6,28 @@ import { STORAGE_ID_NONE_FUNCTION } from '../CellUtils';
 type VertexType = {
   type: string,
   name: string,
-  color: string,
   style: string,
   html: string
 };
 
+const BORDER_STYLE = '1px solid #D0D0D0';
+const VERTEX_TYPES: VertexType[] = [
+  {type: 'consequence', name: 'attackGraphs.consequence', style: 'width:20px;height:12px;border:1px solid black;border-radius:5px;background:#FFFFFF;', html: '&nbsp;'},
+  {type: 'activity_w', name: 'attackGraphs.activity', style: 'width:20px;height:12px;border:1px solid black;background:#FFFFFF;', html: '&nbsp;'},
+  {type: 'activity_g', name: 'attackGraphs.activity', style: 'width:20px;height:12px;border:1px solid black;background:#D7E3BF;', html: '&nbsp;'},
+  {type: 'activity_y', name: 'attackGraphs.activity', style: 'width:20px;height:12px;border:1px solid black;background:#FEE599;', html: '&nbsp;'},
+  {type: 'measurement', name: 'attackGraphs.control', style: 'width:20px;height:12px;border:1px solid black;background:#DAE8FC;', html: '&nbsp;'},
+  {type: 'or', name: 'attackGraphs.orNode', style: '', html: 'OR'},
+  {type: 'and', name: 'attackGraphs.andNode', style: '', html: 'AND'}
+];
+
 export abstract class FunctionListDialog extends SettingsDialog<AttackgraphFunction[]> {
   protected width = 360;
-  protected height = 210;
+  protected height = 360;
   protected title: string | null = null;
   protected editDialogTitle: string | null = null;
 
   private items: AttackgraphFunction[] = [];
-  private vertexTypes: VertexType[] = [
-    {type: 'consequence', name: mxResources.get('attackGraphs.consequence'), color: '#FFFFFF', style: 'width:20px;height:12px;border:1px solid black;border-radius:5px;', html: '&nbsp;'},
-    {type: 'activity_w', name: mxResources.get('attackGraphs.activity'), color: '#FFFFFF', style: 'width:20px;height:12px;border:1px solid black;', html: '&nbsp;'},
-    {type: 'activity_g', name: mxResources.get('attackGraphs.activity'), color: '#D7E3BF', style: 'width:20px;height:12px;border:1px solid black;', html: '&nbsp;'},
-    {type: 'activity_y', name: mxResources.get('attackGraphs.activity'), color: '#FEE599', style: 'width:20px;height:12px;border:1px solid black;', html: '&nbsp;'},
-    {type: 'measurement', name: mxResources.get('attackGraphs.control'), color: '#DAE8FC', style: 'width:20px;height:12px;border:1px solid black;', html: '&nbsp;'},
-    {type: 'or', name: 'OR', color: '#FFFFFF', style: '', html: 'OR'},
-    {type: 'and', name: 'AND', color: '#FFFFFF', style: '', html: 'AND'}
-  ];
 
   abstract getFunctionItems(): AttackgraphFunction[];
   abstract setFunctionItems(aggregationFunctions: AttackgraphFunction[]): void;
@@ -38,7 +39,7 @@ export abstract class FunctionListDialog extends SettingsDialog<AttackgraphFunct
     top.appendChild(title);
 
     const inner = document.createElement('div');
-    inner.style.height = '120px';
+    inner.style.height = '270px';
     inner.style.overflow = 'auto';
     top.appendChild(inner);
 
@@ -65,6 +66,7 @@ export abstract class FunctionListDialog extends SettingsDialog<AttackgraphFunct
           const row = document.createElement('tr');
           const td = document.createElement('td');
           td.style.padding = '0 5px';
+          td.style.borderRight = BORDER_STYLE;
           const span = document.createElement('span');
           span.style.whiteSpace = 'nowrap';
 
@@ -122,10 +124,12 @@ export abstract class FunctionListDialog extends SettingsDialog<AttackgraphFunct
         // 'None' function
         const row = document.createElement('tr');
         const td = document.createElement('td');
-        td.setAttribute('style', 'padding:5px;')
+        td.style.padding = '5px';
+        td.style.borderRight = BORDER_STYLE;
+        td.style.textAlign = 'center';
         td.innerText = mxResources.get('attackGraphs.none');
         row.appendChild(td);
-        this.appendDefaultFunctionSelectFormToRow(row, STORAGE_ID_NONE_FUNCTION, this.vertexTypes.map(e => e.type).filter(e => !defaultTypes.includes(e)));
+        this.appendDefaultFunctionSelectFormToRow(row, STORAGE_ID_NONE_FUNCTION, VERTEX_TYPES.map(e => e.type).filter(e => !defaultTypes.includes(e)));
         tbody.appendChild(row);
 
         table.appendChild(thead);
@@ -171,44 +175,57 @@ export abstract class FunctionListDialog extends SettingsDialog<AttackgraphFunct
 
   private createDefaultFunctionSelectFormHeader(thead: HTMLTableSectionElement) : void {
     const row = document.createElement('tr');
-    const emptyCell = document.createElement('td'); // Empty cell for title of function
-    emptyCell.style.padding = '5px';
-    emptyCell.innerHTML = '&nbsp;';
-    row.appendChild(emptyCell);
-
-    for (let i = 0; i < this.vertexTypes.length; i++) {
+    for (let i = 0; i < VERTEX_TYPES.length; i++) {
       const td = document.createElement('td');
-      td.style.background = this.vertexTypes[i].color;
-      td.style.borderLeft = '1px solid #E0E0E0';
+      td.style.borderRight = BORDER_STYLE;
+      td.style.borderBottom = BORDER_STYLE;
       td.style.padding = '5px';
 
       const div = document.createElement('div');
-      div.setAttribute('style', this.vertexTypes[i].style);
-      div.setAttribute('title', this.vertexTypes[i].name);
-      div.innerHTML = this.vertexTypes[i].html;
+      div.setAttribute('style', VERTEX_TYPES[i].style);
+      div.setAttribute('title', mxResources.get(VERTEX_TYPES[i].name));
+      div.innerHTML = VERTEX_TYPES[i].html;
 
       td.appendChild(div);
       row.appendChild(td);
     }
 
+    const headingRow = document.createElement('tr');
+    const emptyCell = document.createElement('td'); // Empty cell for title of function
+    emptyCell.rowSpan = 2;
+    emptyCell.style.padding = '5px';
+    emptyCell.style.borderRight = BORDER_STYLE;
+    emptyCell.style.borderBottom = BORDER_STYLE;
+    emptyCell.innerHTML = '&nbsp;';
+    headingRow.appendChild(emptyCell);
+
+    const heading = document.createElement('td');
+    heading.colSpan = VERTEX_TYPES.length;
+    heading.style.textAlign = 'center';
+    heading.style.padding = '5px';
+    heading.style.borderRight = BORDER_STYLE;
+    heading.style.borderBottom = BORDER_STYLE;
+    heading.innerText = mxResources.get('attackGraphs.defaultFunction');
+    headingRow.appendChild(heading);
+
+    thead.appendChild(headingRow);
     thead.appendChild(row);
   }
 
   private appendDefaultFunctionSelectFormToRow(row: HTMLTableRowElement, id: string, types: string[]): void {
-    for (let i = 0; i < this.vertexTypes.length; i++) {
+    for (let i = 0; i < VERTEX_TYPES.length; i++) {
       const td = document.createElement('td');
-      td.style.background = this.vertexTypes[i].color;
-      td.style.borderLeft = '1px solid #E0E0E0';
+      td.style.borderRight = BORDER_STYLE;
       td.style.padding = '0 5px';
 
       const input = document.createElement('input');
       input.type = 'radio';
-      input.name = this.vertexTypes[i].type;
+      input.name = VERTEX_TYPES[i].type;
       input.className = id;
       input.addEventListener('click', () => {
         this.updateDefaultFunctions();
       });
-      if (types.includes(this.vertexTypes[i].type)) {
+      if (types.includes(VERTEX_TYPES[i].type)) {
         input.checked = true;
       }
 
